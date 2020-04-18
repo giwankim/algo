73896f1bf5458288255441f680b95ae2c6d870c8
Giwan <giwan.kim@gmail.com>
2020-04-18 18:25:14 +0900

    Leetcode minimum path sum -- more memory efficient

--- leetcode/min-path-sum/MinPathSum.cpp
+++ leetcode/min-path-sum/MinPathSum.cpp
@@ -5,25 +5,26 @@ int minPathSum(vector<vector<int>> &grid)
 {
     int n = (int)grid.size();
     int m = (int)grid[0].size();
-    vector<vector<int>> T(n, vector<int>(m));
-    T[0][0] = grid[0][0];
-    for (int j = 1; j < m; ++j)
+    vector<int> T(m);
+    T[m - 1] = grid[n - 1][m - 1];
+    for (int j = m - 2; j >= 0; --j)
     {
-        T[0][j] = T[0][j-1] + grid[0][j];
+        T[j] = T[j + 1] + grid[n - 1][j];
     }
-    for (int i = 1; i < n; ++i) {
-        T[i][0] = T[i-1][0] + grid[i][0];
-        for (int j = 1; j < m; ++j)
+    for (int i = n - 2; i >= 0; --i)
+    {
+        T[m - 1] = T[m - 1] + grid[i][m - 1];
+        for (int j = m - 2; j >= 0; --j)
         {
-            T[i][j] = min(T[i][j-1], T[i-1][j]) + grid[i][j];
+            T[j] = min(T[j + 1], T[j]) + grid[i][j];
         }
     }
-    return T[n - 1][m - 1];
+    return T[0];
 }
 
 int main(int argc, char const *argv[])
 {
-    vector<vector<int>> grid = {{1,3,1},{1,5,1},{4,2,1}};
+    vector<vector<int>> grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
     cout << minPathSum(grid) << "\n";
     return 0;
 }
