5582ba1f47240a82d841df8b29c0dc454d62a1bd
Giwan Kim <giwan.kim@gmail.com>
2020-04-25 13:44:53 +0900

    Leetcode: LRU cache in python.

---
+++ leetcode/lru-cache/LRUCache.py
@@ -0,0 +1,40 @@
+import collections
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = collections.OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+
+if __name__ == '__main__':
+    cache = LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    assert cache.get(1) == 1
+    cache.put(3, 3)
+    assert cache.get(2) == -1
+    cache.put(4, 4)
+    assert cache.get(1) == -1
+    assert cache.get(3) == 3
+    assert cache.get(4) == 4
+
+    cache = LRUCache(2)
+    cache.put(2, 1)
+    cache.put(1, 1)
+    cache.put(2, 3)
+    cache.put(4, 1)
+    assert cache.get(1) == -1
+    assert cache.get(2) == 3
