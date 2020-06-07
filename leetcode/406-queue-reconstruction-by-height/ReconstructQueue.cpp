21a57e0a1f58242a97a9dc45da36515b36a10deb
Giwan Kim <giwan.kim@gmail.com>
2020-06-07 13:46:07 +0900

    Leetcode: fast STL solution for 406

--- leetcode/406-queue-reconstruction-by-height/ReconstructQueue.cpp
+++ leetcode/406-queue-reconstruction-by-height/ReconstructQueue.cpp
@@ -1,32 +1,28 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+void print_(const vector<vector<int>> &arr)
+{
+    for (auto &p : arr)
+    {
+        cout << "(" << p[0] << "," << p[1] << ") ";
+    }
+    cout << endl;
+}
+
 vector<vector<int>> reconstructQueue(vector<vector<int>> &people)
 {
     sort(people.begin(), people.end(), [](const auto &lhs, const auto &rhs) {
-        return lhs[0] == rhs[0] ? lhs[1] > rhs[1] : lhs[0] < rhs[0];
+        return lhs[0] == rhs[0] ? lhs[1] < rhs[1] : lhs[0] > rhs[0];
     });
-    vector<vector<int>> ans(people.size(), vector<int>(2, -1));
+    list<vector<int>> ll;
     for (auto &p : people)
     {
-        int cnt = p[1];
-        for (auto &q : ans)
-        {
-            if (q[0] == -1)
-            {
-                if (cnt == 0)
-                {
-                    q = p;
-                    break;
-                }
-                else
-                {
-                    cnt -= 1;
-                }
-            }
-        }
+        auto it = ll.begin();
+        advance(it, p[1]);
+        ll.insert(it, p);
     }
-    return ans;
+    return vector<vector<int>>(ll.begin(), ll.end());
 }
 
 int main(int argc, char const *argv[])
@@ -37,11 +33,7 @@ int main(int argc, char const *argv[])
              << " =====\n";
         vector<vector<int>> people{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
         vector<vector<int>> result = reconstructQueue(people);
-        for (auto &p : result)
-        {
-            cout << "(" << p[0] << "," << p[1] << ") ";
-        }
-        cout << endl;
+        print_(result);
         // (5,0) (7,0) (5,2) (6,1) (4,4) (7,1)
 
         vector<vector<int>> correct_result = {
@@ -56,11 +48,7 @@ int main(int argc, char const *argv[])
         vector<vector<int>> people{
             {2, 4}, {3, 4}, {9, 0}, {0, 6}, {7, 1}, {6, 0}, {7, 3}, {2, 5}, {1, 1}, {8, 0}};
         vector<vector<int>> result = reconstructQueue(people);
-        for (auto &p : result)
-        {
-            cout << "(" << p[0] << "," << p[1] << ") ";
-        }
-        cout << endl;
+        print_(result);
         // {{6,0}, {1,1}, {8,0}, {7,1}, {9,0}, {2,4}, {0,6}, {2,5}, {3,4}, {7,3}}
 
         vector<vector<int>> correct_result = {
