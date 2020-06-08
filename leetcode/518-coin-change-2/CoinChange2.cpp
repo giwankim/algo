e0c0f00e6e68d0675d3245e7ee404390c3e5ca57
Giwan Kim <giwan.kim@gmail.com>
2020-06-08 11:27:57 +0900

    Leetcode: coin change 2

--- leetcode/518-coin-change-2/CoinChange2.cpp
+++ leetcode/518-coin-change-2/CoinChange2.cpp
@@ -1,21 +1,30 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int change(int amount, vector<int>& coins) {
-    vector<int> T(amount + 1);
-    T[0] = 1;
-    for (int coin : coins) {
-        for (int i = coin; i <= amount; ++i) {
-            T[i] += T[i - coin];
+int change(int amount, vector<int> &coins)
+{
+    int n = coins.size();
+    vector<vector<int>> T(amount+1, vector<int>(n + 1));
+    for (int j = 0; j <= n; ++j) {
+        T[0][j] = 1;
+    }
+    for (int i = 1; i <= amount; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            T[i][j] = T[i][j - 1];
+            int coin = coins[j - 1];
+            if (i - coin >= 0) {
+                T[i][j] += T[i - coin][j];
+            }
         }
     }
-    return T[amount];
+    return T[amount][n];
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     {
         cout << "=====Example 1======\n";
-        vector<int> coins {1,2,5};
+        vector<int> coins{1, 2, 5};
         int amount = 5;
         int result = change(amount, coins);
         int correct_result = 4;
@@ -25,9 +34,12 @@ int main(int argc, char const *argv[]) {
         cout << result << "\n";
         assert(result == correct_result);
     }
+
     {
         cout << "=====Example 2======\n";
-        vector<int> coins {2,};
+        vector<int> coins{
+            2,
+        };
         int amount = 3;
         int result = change(amount, coins);
         int correct_result = 0;
@@ -37,9 +49,12 @@ int main(int argc, char const *argv[]) {
         cout << result << "\n";
         assert(result == correct_result);
     }
+
     {
         cout << "=====Example 3======\n";
-        vector<int> coins {10,};
+        vector<int> coins{
+            10,
+        };
         int amount = 10;
         int result = change(amount, coins);
         int correct_result = 1;
