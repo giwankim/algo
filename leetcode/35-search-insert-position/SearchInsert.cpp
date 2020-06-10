0e6726c1da2381351453f9120f20193f70e377b0
Giwan Kim <giwan.kim@gmail.com>
2020-06-10 16:52:20 +0900

    Add leetcode::search insert position

---
+++ leetcode/35-search-insert-position/SearchInsert.cpp
@@ -0,0 +1,72 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int searchInsert(vector<int> &nums, int target)
+{
+    int lo = 0;
+    int hi = (int)nums.size() - 1;
+    int ans = -1;
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (nums[mid] < target) {
+            lo = mid + 1;
+        } else {
+            ans = mid;
+            hi = mid - 1;
+        }
+    }
+    return ans == -1 ? (int)nums.size() : ans;
+}
+
+int main()
+{
+    {
+        cout << "=====Example 1=====\n";
+        vector<int> nums{1, 3, 5, 6};
+        int target = 5;
+        int result = searchInsert(nums, target);
+        int correct_result = 2;
+        cout << "Expected:\n\t";
+        cout << correct_result << "\n";
+        cout << "Output:\n\t";
+        cout << result << "\n";
+        // assert(result == correct_result);
+    }
+    {
+        cout << "=====Example 2=====\n";
+        vector<int> nums{1, 3, 5, 6};
+        int target = 2;
+        int result = searchInsert(nums, target);
+        int correct_result = 1;
+        cout << "Expected:\n\t";
+        cout << correct_result << "\n";
+        cout << "Output:\n\t";
+        cout << result << "\n";
+        // assert(result == correct_result);
+    }
+    {
+        cout << "=====Example 3=====\n";
+        vector<int> nums{1, 3, 5, 6};
+        int target = 7;
+        int result = searchInsert(nums, target);
+        int correct_result = 4;
+        cout << "Expected:\n\t";
+        cout << correct_result << "\n";
+        cout << "Output:\n\t";
+        cout << result << "\n";
+        // assert(result == correct_result);
+    }
+    {
+        cout << "=====Example 4=====\n";
+        vector<int> nums{1, 3, 5, 6};
+        int target = 0;
+        int result = searchInsert(nums, target);
+        int correct_result = 0;
+        cout << "Expected:\n\t";
+        cout << correct_result << "\n";
+        cout << "Output:\n\t";
+        cout << result << "\n";
+        // assert(result == correct_result);
+    }
+    return 0;
+}
