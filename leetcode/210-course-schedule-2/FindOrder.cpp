8af35935df43d302fd0c5343a6ef206680ad4fa6
Giwan Kim <giwan.kim@gmail.com>
2020-07-19 16:53:50 +0900

    Add course schedule 2 (bfs)

--- leetcode/210-course-schedule-2/FindOrder.cpp
+++ leetcode/210-course-schedule-2/FindOrder.cpp
@@ -1,11 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-enum Color {UNVISITED, VISITING, VISITED};
-vector<vector<int>> adj;
-vector<Color> colors;
-vector<int> order;
-
 void _print(vector<int>& vec) {
     for (int x : vec) {
         cout << x << " ";
@@ -13,42 +8,39 @@ void _print(vector<int>& vec) {
     cout << "\n";
 }
 
-bool dfs(int i) {
-    colors[i] = VISITING;
-    for (int j = 0; j < adj[i].size(); ++j) {
-        int to = adj[i][j];
-        if (colors[to] == VISITING) {
-            return false;
-        }
-        if (colors[to] == UNVISITED) {
-            if (dfs(to) == false) {
-                return false;
-            }
-        }
-    }
-    colors[i] = VISITED;
-    order.push_back(i);
-    return true;
-}
-
 vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-    adj.assign(numCourses, vector<int>());
+    vector<vector<int>> adj(numCourses);
+    vector<int> indeg(numCourses);
     for (int i = 0; i < prerequisites.size(); ++i) {
         int from = prerequisites[i][0];
         int to = prerequisites[i][1];
         adj[from].push_back(to);
+        indeg[to] += 1;
     }
-    order.clear();
-    colors.assign(numCourses, UNVISITED);
+    queue<int> q;
     for (int i = 0; i < numCourses; ++i) {
-        if (colors[i] == UNVISITED) {
-            colors[i] = VISITING;
-            if (dfs(i) == false) {
-                return vector<int>();
+        if (indeg[i] == 0) {
+            q.push(i);
+        }
+    }
+    vector<int> ret;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        ret.push_back(v);
+        for (int j = 0; j < adj[v].size(); ++j) {
+            int u = adj[v][j];
+            indeg[u] -= 1;
+            if (indeg[u] == 0) {
+                q.push(u);
             }
         }
     }
-    return order;
+    if (ret.size() < numCourses) {
+        return vector<int>();
+    }
+    reverse(ret.begin(), ret.end());
+    return ret;
 }
 
 int main(int argc, char const *argv[]) {
@@ -64,11 +56,11 @@ int main(int argc, char const *argv[]) {
         _print(result);
     }
     {
-        cout << "=====Example 1=====\n";
-        int numCourses = 2;
-        vector<vector<int>> prerequisites{{1, 0}};
+        cout << "=====Example 2=====\n";
+        int numCourses = 4;
+        vector<vector<int>> prerequisites{{1, 0}, {2, 0}, {3, 1}, {3, 2}};
         vector<int> result = findOrder(numCourses, prerequisites);
-        vector<int> correct_result{0, 1};
+        vector<int> correct_result{0, 2, 1, 3};
         cout << "Expected:\n";
         _print(correct_result);
         cout << "Output:\n";
